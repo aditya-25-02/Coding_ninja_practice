d79493605d3b8eacfa8b3d667a0b2acec4e913ff
Aditya <adityaverma3471@gmail.com>
2020-03-14 21:41:29 +0530

    First commit

---
+++ Pattern_ify.cpp
@@ -0,0 +1,13 @@
+void printPatt(int n)
+{
+    for( int i=0 ; i<n ; i++)
+      {
+         for( int j=i ; j<n ; j++)
+           {
+              cout<<"*";  
+           }
+              
+              cout<<"\n";
+      }    
+}
+
