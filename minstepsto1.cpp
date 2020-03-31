ca81dc6ff8fa4af5608a3ec10b8516a48eb7662a
Aditya <adityaverma3471@gmail.com>
2020-03-31 19:34:21 +0530

    Recursion EASY

---
+++ minstepsto1.cpp
@@ -0,0 +1,49 @@
+// Aditya verma 
+#include <iostream>
+#include<string>
+#include<math.h>
+#define FASTIO ios_base::sync_with_stdio(false);std::cin.tie(NULL);std::cout.tie(NULL)
+#define rep(i,a,n) for( ll i=a ; i<n ; i++)
+#define per(i,a,n) for( ll i=n-1 ; i>=a ;i--)
+#define pb push_back
+#define TLE cerr<<"Time elapsed : "<<1000*(ld)clock()/(ld)CLOCKS_PER_SEC<<"ms\n"
+#define vi vector<int>
+#define ll long long int 
+#define pii pair <int,int> 
+#define db double
+#define deb(x) cerr<<#x<<" : "<<x<<"\n";
+#define all(x) x.begin(),x.end()
+ll min(ll a,ll b){ return a<b ? a:b;}
+ll max(ll a,ll b){ return b>a ? b:a;}
+ll gcd(ll a,ll b){ return b?gcd(b,a%b):a;}
+const ll mod=1000000007;
+
+using namespace std;
+
+int countStepsTo1(int n){
+    
+    if(n==1) return 0;
+    
+    int c1 , c2 =INT_MAX , c3=INT_MAX ;
+
+               c1 = countStepsTo1(n-1);
+    if(n%2==0) c2 = countStepsTo1(n/2);
+    if(n%3==0) c3 = countStepsTo1(n/3);
+
+    return 1+min(c1,min(c2,c3));
+}    
+
+
+int main() {
+    FASTIO;
+  	#ifndef ONLINE_JUDGE
+  	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	freopen("error.txt","w",stderr);
+	#endif
+    int n ;
+    cin>>n;
+    int result = countStepsTo1(n);
+    cout<<result<<"\n";
+    return 0 ;
+}
